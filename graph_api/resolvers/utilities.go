3dba8a0c130c9ef6f65bfb3c8ec1579296d69882
myk4040okothogodo <mikeogodo5@gmail.com>
2022-06-10 11:28:08 +0300

    v6.8

--- graph_api/resolvers/utilities.go
+++ graph_api/resolvers/utilities.go
@@ -30,7 +30,7 @@ func graph2ServiceOrderInput(orderInput *model.OrderInput) *service_bv1.Order {
 
 
 func softDeference(field string) string {
-    if field == nil {
+    if field == "" {
         return ""
     }
     return *field
